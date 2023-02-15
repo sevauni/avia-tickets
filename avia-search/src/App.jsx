99e6ff522ec0189826e5f7282ac43fdb267a1cc6
Seva <sevauni@gmail.com>
2023-02-16 01:43:43 +0200

    first commit

---
+++ avia-search/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+//import reactLogo from './assets/react.svg';
+import './App.scss';
+
+
+
+import imgBG from './assets/img/search-bg.png';
+import videoBG from './assets/search-vbg.webm';
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="container">     
+    asdasd 
+      {/* <video className="search-area__bg" playsInline autoPlay muted  poster={imgBG} id="bgvid">
+        <source src={videoBG} type="video/webm" />
+      </video> */}
+    </div>
+  )
+}
+
+export default App
